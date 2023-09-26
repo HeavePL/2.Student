72d7fa517c0c14af01ffcef90feca4355a796427
HeavePL <145488191+HeavePL@users.noreply.github.com>
2023-09-26 20:09:54 +0200

    First

---
+++ Main.java
@@ -0,0 +1,10 @@
+public class Main {
+    public static void main(String[] args) {
+        Student [] klasa4 = new Student[5];
+        klasa4[0] = new Student("Karol", "Marks", 2, 'M', "Informatyk");
+        klasa4[1] = new Student("Marcin", "Grabi", 3, 'M', "Informatyk");
+        klasa4[2] = new Student("Andrzej", "Cygala", 1, 'M', "Informatyk");
+        klasa4[3] = new Student("Władysław", "Brala", 2, 'M', "Informatyk");
+        klasa4[4] = new Student("Jędrzej", "Infernus", 5, 'M', "Informatyk");
+    }
+}
---
+++ Student.java
@@ -0,0 +1,67 @@
+import java.util.Scanner;
+
+public class Student {
+    
+
+    public Student(String imie, String nazwisko,double ocena, char plec, String kierunek){
+
+        WyswietlInformacje();
+        ZmienImie(imie);   
+        ZmienNazwisko(nazwisko);
+        UstawePlec(plec);
+        ZmienOcene(ocena);
+        ZmienKierunek(kierunek);
+        SprawdzZaliczenie();
+
+}
+    
+    String imie;
+    String nazwisko;
+    double ocena;
+    char plec;
+    String kierunek;
+
+    
+
+    public void WyswietlInformacje(){
+        System.out.println(imie);
+        System.out.println(nazwisko);
+        System.out.println(plec);
+        System.out.println(ocena);
+        System.out.println(kierunek);
+    }
+    public void ZmienKierunek(String nowyKierunek){
+        kierunek = nowyKierunek;
+        WyswietlInformacje();
+    }
+
+    public void ZmienOcene(double nowaOcena){
+        ocena = nowaOcena;
+
+        WyswietlInformacje();
+    }
+
+    public void ZmienImie(String noweImie){
+        imie = noweImie;
+    }
+    public void ZmienNazwisko(String noweNazwisko){
+        nazwisko = noweNazwisko;
+    }
+
+    public void UstawePlec(char nowaPlec){
+        plec = nowaPlec;
+    }
+
+    public Boolean SprawdzZaliczenie(){
+
+        if(ocena<2){
+            System.out.println("nie zaliczenie");
+            return false;
+            }
+        System.out.println("zaliczenie");
+        return true;
+    }
+
+
+
+}
